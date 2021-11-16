cb7d077d62807d328f3541fce2cdc13a87495466
Faith <fnbiketi@gmail.com>
2021-11-15 16:23:20 -0800

    infinite scroll

---
+++ base-infinite-swapi/src/species/InfiniteSpecies.jsx
@@ -0,0 +1,41 @@
+  import InfiniteScroll from "react-infinite-scroller";
+import { useInfiniteQuery } from "react-query";
+import { Species } from "./Species";
+
+const initialUrl = "https://swapi.dev/api/species/";
+const fetchUrl = async (url) => {
+  const response = await fetch(url);
+  return response.json();
+};
+
+export function InfiniteSpecies() {
+  const {data,fetchNextPage,hasNextPage,isLoading,isError,error,isFetching}=useInfiniteQuery(
+    'sw-people',
+    ({pageParam =initialUrl})=>fetchUrl(pageParam),{
+      getNextPageParam:(lastPage)=>lastPage.next||undefined 
+    }
+  )
+  if (isLoading) return <div className="loading">Loading ...</div>
+  if(isError) return <div>Error {error.toString()}</div>
+  // TODO: get data for InfiniteScroll via React Query
+  return (
+    <>
+    {isFetching && <div className="loading">Loads...</div>}
+   <InfiniteScroll loadMore={fetchNextPage} hasMore={hasNextPage} />
+     {data.pages.map((pageData)=>{
+      return pageData.results.map((species) =>{
+    return (
+      <Species
+      key={species.name} 
+      name={species.name} 
+      language={species.language}
+      averageLifespan ={species.averageLifespan}
+      />
+      )  
+  })
+})}
+
+  <InfiniteScroll /> 
+  </> 
+  ) 
+}
